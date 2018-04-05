244fa4e52a43fdb3012b884fd8beebb2d8335326
kentyku <kentyku@box>
2018-04-05 23:14:05 +0400

    добавлен сервлет SelectContry формирующий выпадающий список стран, считываемых из БД

---
+++ src/main/java/com/mycompany/mavenservlets/SelectCountry.java
@@ -0,0 +1,123 @@
+/*
+ * Use and copying for commercial purposes
+ * only with the author's permission
+ */
+package com.mycompany.mavenservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kentyku
+ */
+@WebServlet(name = "SelectCountry", urlPatterns = {"/SelectCountry"})
+public class SelectCountry extends HttpServlet {
+    CountriesTableReader ctr;    
+    private ArrayList<Country> countriesList=new ArrayList<Country>();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SelectCountry</title>");            
+            out.println("</head>");
+            out.println("<body>");
+//            out.println("<h1>Servlet SelectCountry at " + request.getContextPath() + "</h1>");
+            out.println("<br>");
+            out.println("<br>");
+            
+            //читаем из БД список стран
+            ctr=new CountriesTableReader();   
+            countriesList=ctr.readData();  
+            
+            //формируем  выпадающий список
+            out.println("<form action=\"ShowCities\" method=\"post\">");
+            out.println("<p><select size=\""+(countriesList.size()+1)+"\" multiple name=\"hero[]\">");  
+            out.println("<option disabled>Выберите страну</option>");                    
+            
+            for (Country itemcountry: countriesList){
+                out.println("<option value=\""+itemcountry.getName()+"\">"+itemcountry.getName()+"</option>");     
+            }                 
+            out.println("</select>");    
+            out.println("<input type=\"submit\" value=\"Выбрать\"></p>");    
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SelectCountry.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(SelectCountry.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SelectCountry.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(SelectCountry.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
