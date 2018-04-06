03d579ed2c5e8314b747d0f2b1a27f19c8d9463f
user <user@debian.user-home>
2018-04-07 01:19:41 +0400

    Подключил внешнюю БД в метод connect

--- src/main/java/com/mycompany/mavenservlets/CountriesTableReader.java
+++ src/main/java/com/mycompany/mavenservlets/CountriesTableReader.java
@@ -66,7 +66,8 @@ public class CountriesTableReader {
                 образом т.к. DriverManager.getConnection() ищет драйверы среди
                 загруженных классов, а не пытается загрузить их сам.
                 */
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase?useSSL=no&serverTimezone=UTC","root","123456");
+//                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase?useSSL=no&serverTimezone=UTC","root","123456");
+                connection = DriverManager.getConnection("jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11231124?useSSL=no&serverTimezone=UTC","sql11231124","T7UI6DZqye");
                 stmt = connection.createStatement();
             }
             catch (SQLException ex) {              
