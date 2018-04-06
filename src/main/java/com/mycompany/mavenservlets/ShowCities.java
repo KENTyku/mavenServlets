6d4049b1683c4f38892e1e31aae914a1dd0aba5a
user <user@debian.user-home>
2018-04-06 22:04:14 +0400

    Сервлет выводящий обрабатывающий метод post от выпадающего списка

--- src/main/java/com/mycompany/mavenservlets/ShowCities.java
+++ src/main/java/com/mycompany/mavenservlets/ShowCities.java
@@ -6,6 +6,10 @@ package com.mycompany.mavenservlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +22,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "ShowCities", urlPatterns = {"/ShowCities"})
 public class ShowCities extends HttpServlet {
+    CountriesTableReader ctr;    
+    private ArrayList<City> cityList=new ArrayList<City>();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,17 +35,29 @@ public class ShowCities extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            String requestDB;
+            requestDB=request.getParameter("country[]");//передача данных запроса
+            //читаем из БД список городов для выбранной страны
+            ctr=new CountriesTableReader();   
+            cityList=ctr.readCities(requestDB);
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet ShowCities</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ShowCities at " + request.getContextPath() + "</h1>");
+//            out.println("<h1>Servlet ShowCities at " + request.getContextPath() + "</h1>");
+            out.println("<br>");
+            
+             //выводим на экран то что прочитали         
+            for (City itemcity: cityList) {
+                out.println("<h1>"+itemcity.getNameCity()+"</h1>");
+            }
             out.println("</body>");
             out.println("</html>");
         }
@@ -57,7 +75,13 @@ public class ShowCities extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ShowCities.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ShowCities.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -71,7 +95,13 @@ public class ShowCities extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ShowCities.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ShowCities.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
