ea502d6397ec596332711ad1a218fcfd4fa052fc
user <user@debian.user-home>
2018-04-06 21:56:06 +0400

    Добавлен простой объект City для передачи списка городов из БД

---
+++ src/main/java/com/mycompany/mavenservlets/City.java
@@ -0,0 +1,43 @@
+/*
+ * Use and copying for commercial purposes
+ * only with the author's permission
+ */
+package com.mycompany.mavenservlets;
+
+/**
+ *
+ * @author user
+ */
+public class City {
+    private String nameCity;
+    private String nameCountry;
+
+    /**
+     * @return the nameCity
+     */
+    public String getNameCity() {
+        return nameCity;
+    }
+
+    /**
+     * @param nameCity the nameCity to set
+     */
+    public void setNameCity(String nameCity) {
+        this.nameCity = nameCity;
+    }
+
+    /**
+     * @return the nameCountry
+     */
+    public String getNameCountry() {
+        return nameCountry;
+    }
+
+    /**
+     * @param nameCountry the nameCountry to set
+     */
+    public void setNameCountry(String nameCountry) {
+        this.nameCountry = nameCountry;
+    }
+    
+}
