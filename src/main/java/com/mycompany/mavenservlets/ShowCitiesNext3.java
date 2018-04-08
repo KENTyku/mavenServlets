e1de49b9f65eb517877d2d2b22f3611ea5aa7882
kentyku <kentyku@box>
2018-04-08 21:33:07 +0400

    Реализовал класс который из базы данных выводи лимитированный перечень городов. При этом имеет две формы для кнопок Назад и Вперед, через которые передает параметры для пролистывания выводимых из БД значений(путем замыкания сервлета на самого себя). Имеется проверка по условию для отлавливания нежелательных событий.

---
+++ src/main/java/com/mycompany/mavenservlets/ShowCitiesNext3.java
@@ -0,0 +1,104 @@
+/*
+ * Use and copying for commercial purposes
+ * only with the author's permission
+ */
+package com.mycompany.mavenservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kentyku
+ */
+@WebServlet(name = "ShowCitiesNext3", urlPatterns = {"/ShowCitiesNext3"})
+public class ShowCitiesNext3 extends ShowCities2 {
+    CountriesTableReader3 ctr3;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String requestDB=null;//передаваемое в запрос название страны
+            requestDB=request.getParameter("country[]");//передача данных запроса
+            
+            if(requestDB==null){
+                requestDB=request.getParameter("requestDB");//передача данных запроса
+            }
+            System.out.println("requsetDB="+requestDB);
+            int delta;//смещение запрашиваемых значений для sql запроса limit
+            
+            
+            delta=Integer.parseInt(request.getParameter("delta"));
+            
+            System.out.println("delta="+delta);
+            //читаем из БД список городов для выбранной страны со смещением delta sql команды limit
+            ctr3=new CountriesTableReader3();   
+            cityList=ctr3.readLimitListCities(requestDB, delta);       
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ShowCities3</title>");            
+            out.println("</head>");
+            out.println("<body>");            
+            
+             //выводим на экран то что прочитали         
+            for (City itemcity: cityList) {
+                out.println("<h1>"+itemcity.getNameCity()+"</h1>");
+            }
+            out.println("<br>");
+            
+            out.println("<div>");
+            out.println("<div style=\"float: left;\">");
+            if (delta>0){
+                //            формируем отправку данных по кнопке Назад
+                out.println("<form action=\"ShowCitiesNext3\" method=\"post\">");                    
+                out.println("<input name=\"requestDB\" type=\"hidden\" id=\"hidden\" value=\""+requestDB+"\">");
+                out.println("<input name=\"delta\" type=\"hidden\" id=\"hidden\" value=\""+(delta-5)+"\">");
+                out.println("<input type=\"submit\" value=\"Назад\">");    
+                out.println("</form>");
+            }
+            
+            out.println("</div>");
+            out.println("<div style=\"float: right;\"> ");
+            if (cityList.size()>4){
+    //            формируем отправку данных по кнопке Вперед
+                out.println("<form action=\"ShowCitiesNext3\" method=\"post\">");                    
+                out.println("<input name=\"requestDB\" type=\"hidden\" id=\"hidden\" value=\""+requestDB+"\">");
+                out.println("<input name=\"delta\" type=\"hidden\" id=\"hidden\" value=\""+(delta+5)+"\">");
+                out.println("<input type=\"submit\" value=\"Вперед\">");    
+                out.println("</form>");
+            }
+            out.println("</div>");
+            out.println("</div>");
+            if ((cityList.size()<=0) & (delta==0)) {
+                out.println("В БД нет ни одного города этой страны");
+            } 
+            out.println("<br>");
+            out.println("<br>");
+            out.println("<a href=\"SimplePaging3\" > Выбрать другую страну </a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
