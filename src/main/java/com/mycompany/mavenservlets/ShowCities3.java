1bae337d0919988bfb2f6623ba8c0d864d8abf08
kentyku <kentyku@box>
2018-04-08 21:29:57 +0400

    Добавил форму с отправкой параметров по кнопке Вперед

--- src/main/java/com/mycompany/mavenservlets/ShowCities3.java
+++ src/main/java/com/mycompany/mavenservlets/ShowCities3.java
@@ -7,6 +7,7 @@ package com.mycompany.mavenservlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,29 +36,21 @@ public class ShowCities3 extends ShowCities2 {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            String requestDB;
+            String requestDB=null;//передаваемое в запрос название страны
             requestDB=request.getParameter("country[]");//передача данных запроса
+            int delta=0;//смещение запрашиваемых значений для sql запроса limit
+            
             //читаем из БД список городов для выбранной страны
             ctr3=new CountriesTableReader3();   
-            cityList=ctr3.readLimitListCities(requestDB, 0);
-            
-            //подсчитывем количество необходимых страниц для вывода 5 элементов на страницу
-            int elements=ctr3.readSizeListCities(requestDB);
-            int pages=0;
-            if (elements%5==0) pages=elements/5;        
-            else pages=(elements/5)+1;
-            
-            
-        
-            
-            
+            cityList=ctr3.readLimitListCities(requestDB, delta);
+      
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ShowCities2</title>");            
+            out.println("<title>Servlet ShowCities3</title>");            
             out.println("</head>");
             out.println("<body>");
-//            out.println("<h1>Servlet ShowCities2 at " + request.getContextPath() + "</h1>");
+//            out.println("pages="+pages+"");
             out.println("<br>");
             
              //выводим на экран то что прочитали         
@@ -65,8 +58,22 @@ public class ShowCities3 extends ShowCities2 {
                 out.println("<h1>"+itemcity.getNameCity()+"</h1>");
             }
             out.println("<br>");
-            out.println("<input type=\"submit\" value=\"Назад\">"); 
-            out.println("<input type=\"submit\" value=\"Вперед\">"); 
+            out.println("<div style=\"float: right;\"> ");
+            if (cityList.size()>0){
+    //            формируем отправку данных по кнопке вперед
+                out.println("<form action=\"ShowCitiesNext3\" method=\"post\">");                    
+                out.println("<input name=\"requestDB\" type=\"hidden\" id=\"hidden\" value=\""+requestDB+"\">");
+                out.println("<input name=\"delta\" type=\"hidden\" id=\"hidden\" value=\""+(delta+5)+"\">");
+                out.println("<input type=\"submit\" value=\"Вперед\">");    
+                out.println("</form>");
+            }            
+            out.println("</div>");
+            if (cityList.size()<=0) {
+                out.println("В БД нет ни одного города этой страны");
+            } 
+            out.println("<br>");
+            out.println("<br>");
+            out.println("<a href=\"SimplePaging3\" > Выбрать другую страну </a>");
             out.println("</body>");
             out.println("</html>");
         }
