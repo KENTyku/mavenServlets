59224e00ae8b40154ea616d86bb19d9bf2bf57b8
Olga <Olga@Olga-asus>
2018-07-05 15:50:58 +0200

    Убрал лишний код который выполняется  с помощью скрипта javascript для организации ajax

--- src/main/java/com/mycompany/search_ajax/ShowResultSearchAjax.java
+++ src/main/java/com/mycompany/search_ajax/ShowResultSearchAjax.java
@@ -9,6 +9,7 @@ import com.mycompany.search.*;
 import com.mycompany.select.ShowCities;
 import com.mycompany.showcontrieslist.City;
 import com.mycompany.showcontrieslist.CountriesTableReader;
+import com.mycompany.showcontrieslist.Country;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -24,10 +25,11 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author user
  */
-@WebServlet(name = "ShowResultSearch", urlPatterns = {"/ShowResultSearch"})
+@WebServlet(name = "ShowResultSearchAjax", urlPatterns = {"/ShowResultSearchAjaxServlet"})
 public class ShowResultSearchAjax extends ShowCities {
-    CountriesTableReader ctr5;    
-    ArrayList<City> cities=new ArrayList<>();
+
+    CountriesTableReader ctr5;
+    ArrayList<City> cities = new ArrayList<>();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -44,85 +46,112 @@ public class ShowResultSearchAjax extends ShowCities {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, ClassNotFoundException, SQLException {
         String requestDB;
-        requestDB=null;//передаваемое в запрос название страны
-            requestDB=request.getParameter("id");//считывание данных запроса
-            
-            if(requestDB==null){
-                requestDB=request.getParameter("requestDB");//считывание данных запроса
-            }   
-             //считываем данные из запроса
+        requestDB = null;//передаваемое в запросе название страны
+        //считываем данные из запроса
+        requestDB = request.getParameter("id");//получение из запроса         
+        //скрипта значения поля формы(введенное пользователем значение) 
+
+//            if(requestDB==null){
+//                requestDB=request.getParameter("requestDB");//считывание данных запроса
+//            }   
+        //считываем данные из запроса
         String action = request.getParameter("action");
-        String targetId = request.getParameter("id");//получение из запроса 
-        //скрипта значения поля формы(введенное пользователем значение)
+
+        int index;//смещение запрашиваемых значений для sql запроса limit
+
+        index = Integer.parseInt(request.getParameter("index"));//считывание данных запроса
 
         StringBuffer sb = new StringBuffer();//для временного сохранения строки (для формирования xml)
 
         //читаем из БД
-        ctr = new CountriesTableReader();
+        ctr5 = new CountriesTableReader();
 //            ctr.createdb();
-        
-        countriesList = ctr.readCountries();
-        ctr5=new CountriesTableReader();
-        
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {           
-            int index;//смещение запрашиваемых значений для sql запроса limit
-                        
-            index=Integer.parseInt(request.getParameter("index"));//считывание данных запроса
-            
-            //читаем из БД список городов для выбранной страны начиная с элемента 
-            //index sql команды limit
-            cities=ctr5.searchCitiesOfCountries(requestDB,index);
-                 
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ShowCities</title>");            
-            out.println("</head>");
-            out.println("<body>");            
-            
-             //выводим на экран то что прочитали         
-            for (City itemcity: cities) {
-                out.println("<h1>"+itemcity.getNameCity()+"</h1>");
+
+//        ArrayList<Country> countriesList = ctr5.readCountries();
+
+        if (requestDB != null) {//если значение не нулевое
+            requestDB = requestDB.trim().toLowerCase();//убираем спереди и в конце пробелы, и делаем все буквы прописными
+        } else {
+//            context.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+
+        //читаем из БД список городов для выбранной страны(стран), начиная с элемента 
+        //index sql команды limit
+        cities = ctr5.searchCitiesOfCountries(requestDB, index);
+
+        //формируем ответ в виде xml
+        boolean requestAdded = false;
+
+        if (action.equals("requestComplete")) {//идентифицируем наш запрос из всех возможных запросов
+
+            // проверяем что запрос не пустой
+            if (!requestDB.equals("")) {
+
+                //подготавливаем xml данные
+                for (City itemcity : cities) {
+                    sb.append("<city>");//                      
+                    sb.append("<name>" + itemcity.getNameCity() + "</name>");//                       ;
+                    sb.append("</city>");
+                    requestAdded = true;
+                }
             }
-            out.println("<br>");
-            
-            out.println("<div>");
-            out.println("<div style=\"float: left;\">");
-            
-            if (index>0){//условие показа кнопки Назад формы
-                //            формируем отправку данных по кнопке Назад
-                out.println("<form action=\"ShowResultSearch\" method=\"post\">");                    
-                out.println("<input name=\"requestDB\" type=\"hidden\" id=\"hidden\" value=\""+requestDB+"\">");
-                out.println("<input name=\"index\" type=\"hidden\" id=\"hidden\" value=\""+(index-5)+"\">");
-                out.println("<input type=\"submit\" value=\"Назад\">");    
-                out.println("</form>");
+
+            if (requestAdded) {
+                //если имена добавлены, то отправляем в ответ на запрос xml строку
+                
+                response.setContentType("text/xml");
+                response.setHeader("Cache-Control", "no-cache");
+                response.getWriter().write("<cities>" + sb.toString() + "</cities>");//xml строка
+
+                System.out.println("************");
+//                System.out.println(sb.toString());
+//                System.out.println(requestDB);
+//                System.out.println("************");
+//                System.out.println(request.getProtocol());
+                
+            } else {
+                //nothing to show
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
             }
-            
-            out.println("</div>");            
-            out.println("<div style=\"float: right;\"> ");
-            
-            if (cities.size()>4){ //условие показа кнопки Вперед формы
-    //            формируем отправку данных по кнопке Вперед
-                out.println("<form action=\"ShowResultSearch\" method=\"post\">");                    
-                out.println("<input name=\"requestDB\" type=\"hidden\" id=\"hidden\" value=\""+requestDB+"\">");
-                out.println("<input name=\"index\" type=\"hidden\" id=\"hidden\" value=\""+(index+5)+"\">");
-                out.println("<input type=\"submit\" value=\"Вперед\">");    
-                out.println("</form>");
-            }
-            out.println("</div>");
-            out.println("</div>");
-            
-            if ((cities.size()<=0) & (index==0)) {
-                out.println("В БД нет ни одной страны, соответствующей запросу");
-            } 
-            out.println("<br>");
-            out.println("<br>");
-            out.println("<a href=\"SearchCountryPaging\" > Повторить поиск </a>");
-            out.println("</body>");
-            out.println("</html>");
-        }       
+
+//                out.println("<br>");
+//
+//                out.println("<div>");
+//                out.println("<div style=\"float: left;\">");
+//
+//                if (index > 0) {//условие показа кнопки Назад формы
+//                    //            формируем отправку данных по кнопке Назад
+//                    out.println("<form action=\"ShowResultSearch\" method=\"post\">");
+//                    out.println("<input name=\"requestDB\" type=\"hidden\" id=\"hidden\" value=\"" + requestDB + "\">");
+//                    out.println("<input name=\"index\" type=\"hidden\" id=\"hidden\" value=\"" + (index - 5) + "\">");
+//                    out.println("<input type=\"submit\" value=\"Назад\">");
+//                    out.println("</form>");
+//                }
+//
+//                out.println("</div>");
+//                out.println("<div style=\"float: right;\"> ");
+//
+//                if (cities.size() > 4) { //условие показа кнопки Вперед формы
+//                    //            формируем отправку данных по кнопке Вперед
+//                    out.println("<form action=\"ShowResultSearch\" method=\"post\">");
+//                    out.println("<input name=\"requestDB\" type=\"hidden\" id=\"hidden\" value=\"" + requestDB + "\">");
+//                    out.println("<input name=\"index\" type=\"hidden\" id=\"hidden\" value=\"" + (index + 5) + "\">");
+//                    out.println("<input type=\"submit\" value=\"Вперед\">");
+//                    out.println("</form>");
+//                }
+//                out.println("</div>");
+//                out.println("</div>");
+//
+//                if ((cities.size() <= 0) & (index == 0)) {
+//                    out.println("В БД нет ни одной страны, соответствующей запросу");
+//                }
+//                out.println("<br>");
+//                out.println("<br>");
+//                out.println("<a href=\"SearchCountryPaging\" > Повторить поиск </a>");
+//                out.println("</body>");
+//                out.println("</html>");
+        }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
