5a62a1b963b220b206c61b3d52b2c2d22a60cc7f
Olga <Olga@Olga-asus>
2018-07-09 15:20:34 +0200

    реализовал функции добавления кнопок Назад и Вперёд, но пока добавляются в ячейки таблицы каждый раз как нажимаешь на них

--- src/main/webapp/javascript.js
+++ src/main/webapp/javascript.js
@@ -9,13 +9,14 @@ var completeField;//поле ввода символов
 var completeTable;//таблица, выводящая в ответ список стран
 
 var pagingTable;//таблица для отрисовывания кнопок вперед назад и фразы что запрашиваемых данных нет
-var rowPaging;//создали строку
+var rowPaging = null;//создали строку
 var cellNextPaging;//ячейка для кнопки Вперёд
 var cellBackPaging;//ячейка для кнопки Назад
 var cellInfoPaging;//ячейка для вывода информации
-        
+var replase;//утверждение о том что создана таблица для пейджинга 
+
 var sizecitieslist; //количество городов полученных из ответа на запрос в БД
-var index=0;//значение смещения количества элементов в запросе к БД
+var index = 0;//значение смещения количества элементов в запросе к БД
 //var autoRow;//строка в которую помещаем таблицу completeTable
 
 
@@ -39,8 +40,8 @@ function doCompletion() {
     completeTable = document.getElementById("complete-table");
 //    autoRow = document.getElementById("auto-row");
 //    completeTable.style.top = getElementY(completeTable) + "px";//выравнивание таблицы
-    pagingTable=document.getElementById("paging-table");
-    
+    pagingTable = document.getElementById("paging-table");
+
     /**
      * формируем строку для GET запроса к серверу через сервлет         * 
      * Url состоит из:
@@ -124,7 +125,7 @@ function appendCity(name) {
 //    linkElement.setAttribute("href", "autocomplete?action=lookup&id=");
     textelement.appendChild(document.createTextNode(name));
     cell.appendChild(textelement);
-    
+
 }
 //для выравнивания таблицы предложений
 function getElementY(element) {
@@ -143,7 +144,7 @@ function getElementY(element) {
 }
 //очистка таблицы предложений поиска
 function clearTable() {
-    console.log("log:"+completeTable.getElementsByTagName("tr").length);
+//    console.log("log:"+completeTable.getElementsByTagName("tr").length);
     if (completeTable.getElementsByTagName("tr").length > 0) {
 //        completeTable.style.display = 'none';
         for (var item = completeTable.childNodes.length - 1; item >= 0; item--) {
@@ -162,74 +163,87 @@ function parseMessages(responseXML) {
         //присваиваем первый элемент массива ссылок данного имени, 
         //найденных во всем xml документе
         var cities = responseXML.getElementsByTagName("cities")[0];
-        sizecitieslist=cities.childNodes.length;
+        sizecitieslist = cities.childNodes.length;
         if (cities.childNodes.length > 0) {
 //            console.log(cities.childNodes.length);
             completeTable.setAttribute("bordercolor", "black");
             completeTable.setAttribute("border", "1");
-            completeTable.setAttribute("cellspacing","0");
+            completeTable.setAttribute("cellspacing", "0");
 //            alert("response added");
-            
+
 //            console.log("size="+sizecitieslist);
 
             for (var item = 0; item < (cities.childNodes.length); item++) {
                 var city = cities.childNodes[item];
                 var name = city.getElementsByTagName("name")[0];//хранит ссылку на элемент name
-                
+
                 //добавляем в таблицу html страницы распаресенные данные
 //                alert("TEST");
 //                console.log(cities.childNodes.length);
-                console.log(name.childNodes[0].nodeValue);
+//                console.log(name.childNodes[0].nodeValue);
                 appendCity(name.childNodes[0].nodeValue);
 //                console.log(1000);
             }
             //создаем таблицу для вставки кнопок вперед назад и информации что данные отсутствуют
-            rowPaging=document.createElement("tr");//создали строку
-            cellNextPaging=document.createElement("td");
-            cellBackPaging=document.createElement("td");
-            cellInfoPaging=document.createElement("td");
-            rowPaging.appendChild(cellBackPaging);
-            rowPaging.appendChild(cellInfoPaging);
-            rowPaging.appendChild(cellNextPaging);
-            pagingTable.appendChild(rowPaging);
-            
+            console.log(rowPaging);
+            if (rowPaging == null)               
+            {
+                rowPaging = document.createElement("tr");//создали строку
+                cellNextPaging = document.createElement("td");
+                cellBackPaging = document.createElement("td");
+                cellInfoPaging = document.createElement("td");
+
+                rowPaging.appendChild(cellBackPaging);
+                rowPaging.appendChild(cellInfoPaging);
+                rowPaging.appendChild(cellNextPaging);
+                pagingTable.appendChild(rowPaging);
+            }
+
+
 //            var textButtonNext=document.createTextNode("Вперёд");
-            
-            if (sizecitieslist>4) {
+
+            if (sizecitieslist > 4) {
                 addButtonNext();
             }
-            if(index>0){
+            if (index > 0) {
                 addButtonBack();
             }
-            
+
         }
-        if ((index==0)&&(sizecitieslist<=0)){
+        if ((index == 0) && (sizecitieslist <= 0)) {
             noData();
         }
     }
 }
 
 function addButtonNext() {
-    //<input type="button" value="Поиск" onclick="clickButtonNext();">
-    var buttonNext=document.createElement("input");
-    buttonNext.setAttribute("type","button");
-    buttonNext.setAttribute("value","Вперёд");
-    buttonNext.setAttribute("onclick","clickButtonNext()");
-    cellNextPaging.appendChild(buttonNext);
     //добавляем кнопку Вперед в правой части страницы
+    var buttonNext = document.createElement("input");
+    buttonNext.setAttribute("type", "button");
+    buttonNext.setAttribute("value", "Вперёд");
+    buttonNext.setAttribute("onclick", "clickButtonNext()");
+    if (rowPaging!=null)cellNextPaging.replaceChild(buttonNext);
+    else cellNextPaging.appendChild(buttonNext);
+
 }
 function clickButtonNext() {
-    index=index+5;
-    doCompletion();    
+    index = index + 5;
+    doCompletion();
 }
 
 function addButtonBack() {
     //<input type="button" value="Поиск" onclick="clickButtonBack();">
     //добавляем кнопку Назад в левой части страницы
+    var buttonBack = document.createElement("input");
+    buttonBack.setAttribute("type", "button");
+    buttonBack.setAttribute("value", "Назад");
+    buttonBack.setAttribute("onclick", "clickButtonBack()");
+    if (rowPaging!=null)cellBackPaging.replaceChild(buttonBack);
+    else cellBackPaging.appendChild(buttonBack);
 }
 function clickButtonBack() {
-    index=index-5;
-    doCompletion();    
+    index = index - 5;
+    doCompletion();
 }
 
 function noData() {
